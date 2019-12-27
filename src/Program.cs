1ee9f6de9976b212b42ce4f5e6474ef80d8d2b1e
PatrikBichis <patrik.olsson@x-firm.com>
2019-12-27 16:29:07 +0100

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -29,6 +29,8 @@ namespace dotnet_core_reaction_speed_game
             // Add event to clean up on exit
             AppDomain.CurrentDomain.ProcessExit += new EventHandler(CurrentDomain_ProcessExit); 
 
+            TestProgram();
+
             game = new ReactionSpeedGame(simulate,mqttInterface);
 
             game.StartGame();
@@ -40,6 +42,23 @@ namespace dotnet_core_reaction_speed_game
             Console.WriteLine("Starting to clean up...");
             game.CleanUp();
             Console.WriteLine("Clean up is done, closing application");
+        }
+
+        static void TestProgram(){
+            Console.WriteLine("Test program, pressa buttons with ligth on");
+            var controller = new GpioSerivce();
+            controller.Init();
+            controller.SetLedState(enums.LedType.LED1, true);
+            controller.WaitForButtonPressInGame(0);
+            controller.SetLedState(enums.LedType.LED2, true);
+            controller.WaitForButtonPressInGame(1);
+            controller.SetLedState(enums.LedType.LED3, true);
+            controller.WaitForButtonPressInGame(2);
+            controller.SetLedState(enums.LedType.LED4, true);
+            controller.WaitForButtonPressInGame(3);
+            controller.SetLedState(enums.LedType.LED5, true);
+            controller.WaitForButtonPressInGame(4);
+            controller.Close();
         }
     }
 }
